41cbb5265bfc7d81ddb41727de259be2cfcd3891
Diego5212 <124754726+robocay1252@users.noreply.github.com>
2023-04-25 12:02:42 -0600

    Add files via upload

---
+++ Practica10FigurasGeo/src/practica10figurasgeo/Datos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica10figurasgeo;
+
+/**
+ *
+ * @author SAC2-5
+ */
+public class Datos {
+    
+}
